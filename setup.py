833666d6997aa2fbfefc071d980e9ce488b9e1b3
Hagabooga <hagaboogaplease@gmail.com>
2016-08-01 03:57:24 -0700

    setuip change

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ executables = [cx_Freeze.Executable("welcome-of-fate.py")]
 cx_Freeze.setup(
     name="Booga's Welcome of Fate",
     options={"build_exe":\
-             {"packages":["pygame",'random','math','time',\
-                          'actions','images','sounds','items',\
+             {"packages":["pygame",'random','math','time','shelve','os.path',\
+                          'actions','colors','images','items',\
                           'jobs','player','potions','sounds',\
                           'utilities','weapons']}},
     executables = executables

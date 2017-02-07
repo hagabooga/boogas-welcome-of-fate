c608ab5f3eafdba14a51c60f01c48ba5d5d75ac6
Cameron <Cameron>
2017-02-07 03:59:44 -0800

    exe release of alpha.v2.1
    please see release section for more info

--- Booga's Welcome of Fate alpha.v2.0/images.py
+++
@@ -1,5 +0,0 @@
-import pygame
-pygame.init()
-small_arrow_left = pygame.image.load('game/general/small_arrow_left.png')
-small_arrow_right = pygame.image.load('game/general/small_arrow_right.png')
-
